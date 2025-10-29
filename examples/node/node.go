41113c695ae7fe279e3f8797a1c28918633eca78
Olle Lögdahl <olle.logdahl@gmail.com>
2025-10-29 10:54:29 +0100

    example: improved the node example with lookup

--- examples/node/node.go
+++ examples/node/node.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"flag"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/ollelogdahl/concord"
@@ -28,20 +31,42 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Server started at", *bindAddr)
 
 	if *joinAddr == "" {
 		server.Create()
+		fmt.Println("Cluster created")
 	} else {
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 		defer cancel()
+
+		fmt.Println("Joining cluster...")
 
 		err := server.Join(ctx, *joinAddr)
 		if err != nil {
 			panic(err)
 		}
+		fmt.Println("Joined cluster")
 	}
 
-	for {
-		time.Sleep(10 * time.Second)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Enter keys to lookup (Ctrl+D to exit):")
+	for scanner.Scan() {
+		key := scanner.Text()
+		if key == "" {
+			continue
+		}
+
+		// Perform lookup
+		value, err := server.Lookup([]byte(key))
+		if err != nil {
+			fmt.Printf("Error looking up '%s': %v\n", key, err)
+		} else {
+			fmt.Printf("'%s' -> %s\n", key, value.Name)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading from stdin: %v\n", err)
 	}
 }
