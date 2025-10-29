7933c1653057fc4248dbaa4c89311d86f8daf858
Olle Lögdahl <olle.logdahl@gmail.com>
2025-10-29 01:38:36 +0100

    improved testing, system test cover all packages

--- test/system/system_test.go
+++ test/system/system_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ollelogdahl/concord"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -33,5 +34,63 @@ func TestBasicClusterFormation(t *testing.T) {
 	assert.EventuallyWithT(t, func(ct *assert.CollectT) {
 		AssertConsistentRing(ct, nodes)
 		AssertConsistentLookupForKey(ct, ctx, nodes, key)
+		AssertFullRangeCover(ct, nodes)
+	}, 10*time.Second, 100*time.Millisecond)
+}
+
+func TestNodeShutdown(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	setup := NewConcordSetup()
+
+	// Create 2 nodes
+	nodes, err := setup.CreateClusterNodes(t, ctx, 3)
+	require.NoError(t, err, "failed to create cluster nodes")
+	defer setup.StopNodes(ctx, nodes)
+
+	// Connect them into a ring
+	err = setup.ConnectCluster(ctx, nodes)
+	require.NoError(t, err, "failed to connect cluster")
+
+	err = nodes[1].Stop()
+	require.NoError(t, err, "failed to stope node 2")
+
+	ns := []*concord.Concord{nodes[0], nodes[2]}
+
+	// Assert ring consistency
+	assert.EventuallyWithT(t, func(ct *assert.CollectT) {
+		AssertConsistentRing(ct, ns)
+		AssertFullRangeCover(ct, ns)
+	}, 10*time.Second, 100*time.Millisecond)
+}
+
+func Test2NodeShutdown(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	setup := NewConcordSetup()
+
+	// Create 2 nodes
+	nodes, err := setup.CreateClusterNodes(t, ctx, 3)
+	require.NoError(t, err, "failed to create cluster nodes")
+	defer setup.StopNodes(ctx, nodes)
+
+	// Connect them into a ring
+	err = setup.ConnectCluster(ctx, nodes)
+	require.NoError(t, err, "failed to connect cluster")
+
+	err = nodes[1].Stop()
+	require.NoError(t, err, "failed to stope node 2")
+
+	err = nodes[2].Stop()
+	require.NoError(t, err, "failed to stope node 2")
+
+	ns := []*concord.Concord{nodes[0]};
+
+	// Assert ring consistency
+	assert.EventuallyWithT(t, func(ct *assert.CollectT) {
+		AssertConsistentRing(ct, ns)
+		AssertFullRangeCover(ct, ns)
 	}, 10*time.Second, 100*time.Millisecond)
 }
--- test/unit/api_test.go
+++ test/unit/api_test.go
@@ -1,1 +1,28 @@
 package unit_test
+
+import (
+	"testing"
+
+	"github.com/ollelogdahl/concord"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetName(t *testing.T) {
+	config := concord.Config{
+		Name: "foo",
+	}
+
+	instance := concord.New(config)
+
+	assert.Equal(t, instance.Name(), "foo")
+}
+
+func TestGetAddress(t *testing.T) {
+	config := concord.Config{
+		AdvAddr: "localhost:1234",
+	}
+
+	instance := concord.New(config)
+
+	assert.Equal(t, instance.Address(), "localhost:1234")
+}
